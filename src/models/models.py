d9510a4cefc16c2c4c184f457df7f24672649bbc
Johannes Schöck <38401818+JSchoeck@users.noreply.github.com>
2024-11-29 03:47:09 +0100

    Messed with the model structure Add settings

--- src/models/models.py
+++ src/models/models.py
@@ -1,34 +1,56 @@
 from typing import Protocol
 
 import pandas as pd
-import xgboost as xgb
+from catboost import CatBoostRegressor
+from xgboost import XGBRegressor
+
+import utils
+
+S = utils.load_settings()
+
 
 class Model(Protocol):
-    def predict(self, data: pd.DataFrame) -> pd.Series: ...
+    def predict(self, data: pd.DataFrame, *args, **kwargs) -> pd.Series: ...
 
 
 class Naive(Model):
     def predict(self, data: pd.DataFrame) -> pd.Series:
-        return pd.Series([1.0] * len(data))
+        return pd.Series([S["naive"]["value"]] * len(data))
 
-class XgBoost(Model):
-    def predict(self, train_data: pd.DataFrame, test_data: pd.DataFrame, numerical_features, categorical_features) -> pd.Series:
-        features = categorical_features + numerical_features
-        X_train = train_data[features].astype({col: "category" for col in categorical_features})
-        y_train = train_data["target"]
-        X_test = test_data[features].astype({col: "category" for col in categorical_features})
 
-        # TODO: tune hyperparameters to achieve better results
-        xgboost = xgb.XGBRegressor(tree_method="hist", max_depth=4, enable_categorical=True)
-        xgboost.fit(X_train, y_train)
-        return xgboost.predict(X_test)
+# class XGBoost(Model):
+#     def preprocess(
+#         self,
+#         train_data: pd.DataFrame,
+#         test_data: pd.DataFrame,
+#         numerical_features: list[str],
+#         categorical_features: list[str],
+#     ) -> pd.DataFrame:
+#         features = categorical_features + numerical_features
+#         X_train = train_data[features].astype({col: "category" for col in categorical_features})
+#         y_train = train_data["target"]
+#         X_test = test_data[features].astype({col: "category" for col in categorical_features})
+
+#         return data
+
+#     def predict(
+#         self,
+#     ) -> pd.Series:
+#         # TODO: tune hyperparameters to achieve better results
+#         xgboost = XGBRegressor(tree_method="hist", max_depth=4, enable_categorical=True)
+#         xgboost.fit(X_train, y_train)
+#         return xgboost.predict(X_test)
 
 
 class CatBoost(Model):
-    def predict(self, train_data: pd.DataFrame,test_data: pd.DataFrame, features) -> pd.Series:
+    def preprocess(self, data: pd.DataFrame) -> pd.DataFrame:
+        return data
+
+    def predict(self, train_data: pd.DataFrame, test_data: pd.DataFrame, features: list[str]) -> pd.Series:
         X_train = train_data[features].astype({col: "category" for col in features})
         y_train = train_data["target"]
         X_test = test_data[features].astype({col: "category" for col in features})
+
         # TODO: tune hyperparameters to achieve better results
         catboost = CatBoostRegressor(
             cat_features=features,
